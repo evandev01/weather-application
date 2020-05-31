6e18ac0c5ae4fbcdbb51dbae3209cc9b7b577676
Evan Bero <evanbero1980@gmail.com>
2020-05-30 20:40:21 -0400

    added updated html, js, css and pseudo code files

---
+++ script.js
@@ -0,0 +1,39 @@
+var apiKey="723e37136fff2bbcd07b95ca1cc52234";
+var queryURL="api.openweathermap.org/data/2.5/forecast?q=";
+
+var cityName="";
+var cityBtns=[""];
+var curWeatherCard="";
+var fiveDayWeatherCards=[""];
+var weatherIcons=[""];
+var date="";
+var temp="";
+var humidity="";
+var windSpeed="";
+var UVindex="";
+
+
+
+
+//  function displayCityWeather(){
+
+//         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=723e37136fff2bbcd07b95ca1cc52234";
+
+//             $.ajax({
+//             url: queryURL,
+//             method: "GET"
+//             }).then(function(response){
+                
+//                 var cityList = $("<div class='city'>");
+//                 var btn = $("<button>");
+//                     btn.addClass("city-btn");
+//                     btn.text(city-input);
+//                 $(response).on("click", ".search-btn", response);
+//                 console.log(response);
+//             })
+//         }
+        
+            
+            
+            
+            
